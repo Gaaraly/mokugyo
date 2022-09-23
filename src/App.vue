18d7ceed92657741ba4612d8c9bd129f3da7345b
Gaaraly <precious.wei@qq.com>
2022-09-23 16:11:42 +0800

    repo init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main
+    font-sans
+    p="x-4 y-10" text="center gray-700 dark:gray-200"
+    h-screen
+  >
+    <div w-full h-full bg-black>
+      Hi
+    </div>
+    <Footer />
+  </main>
+</template>
