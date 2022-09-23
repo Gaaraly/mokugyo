1c60a413f3851b2f86aad82cfbfdf8de34828245
Gaaraly <precious.wei@qq.com>
2022-09-23 16:18:29 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <main
     font-sans
     p="x-4 y-10" text="center gray-700 dark:gray-200"
-    h-screen
+    h-screen w-screen
   >
-    <div w-full h-full bg-black>
+    <div w-full h-full>
       Hi
     </div>
     <Footer />
