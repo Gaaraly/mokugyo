e2bf6f185ccf79d43293a7f06ef4275c4080bce7
Gaaraly <precious.wei@qq.com>
2022-10-06 14:35:22 +0800

    fix: scale err

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@ function handleCilck() {
   >
     <div
       w-full h-full
-      flex justify-center items-center
+      flex justify-center items-center select-none
       @click="handleCilck"
     >
       <!-- Hi -->
       <img
         src="/muyu.png" alt="mokugyo"
         w-auto h-auto max-w-full max-h-full
-        lg:scale-120
+        cursor-pointer
         animate-bounce-in animate-duration-none
         active:animate-none hover:animate-duration-500
       >
