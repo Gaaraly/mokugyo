772c6015840636e37ad8ad35faa5009b708503c9
Gaaraly <precious.wei@qq.com>
2022-10-08 11:34:05 +0800

    useRegisterSW

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+
+useRegisterSW()
 
 const app = createApp(App)
 const router = createRouter({
